6d3ae2cbd9d19bebda63a1b42aa6f476f07d6879
GGGoradia <devgoradia22@gmail.com>
2026-01-13 17:23:17 +0530

    newly added files

---
+++ frontend-landing website/src/components/privateroutes/PrivateRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ allowedRoles }) => {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  // Not logged in
+  if (!isAuthenticated) {
+    return <Navigate to="/bussiness/signup" replace />;
+  }
+
+  // Role check (only if roles are specified)
+  if (
+    allowedRoles &&
+    (!user?.roles || !allowedRoles.some((r) => user.roles.includes(r)))
+  ) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
